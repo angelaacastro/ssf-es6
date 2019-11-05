d139a21c2959b6f3c5f4d093ce7d4496458d7e85
Angela Castro <angelacastro@Angelas-MacBook-Pro.local>
2019-11-04 20:59:32 -0800

    Converted sum function to arrow function

--- challenges/01-arrow-functions/01-sum.js
+++ challenges/01-arrow-functions/01-sum.js
@@ -1,8 +1,9 @@
 //Convert the below function into and ES6 arrow funciton
 
-function sum(a, b) {
-    return a + b;
-}
+// function sum(a, b) {
+//     return a + b;
+// }
+let sum = (a,b) => a + b;
 
 
 
