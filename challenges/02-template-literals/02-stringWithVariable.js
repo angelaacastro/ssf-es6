5752af602796d43f010d816ed8f2a317fed0e2ba
Angela Castro <angelacastro@Angelas-MacBook-Pro.local>
2019-11-06 18:33:42 -0800

    strings with a variable

--- challenges/02-template-literals/02-stringWithVariable.js
+++ challenges/02-template-literals/02-stringWithVariable.js
@@ -1,11 +1,15 @@
 //Convert the string being returned by the below function to utilize template literal syntax.  Be sure to make use of string 
 //interpolation
 
+//const stringWithVariable = _ => {
+    //const CITY = 'Defiance'; 
+  //  return 'Welcome to ' + CITY;
+//};
+
 const stringWithVariable = _ => {
     const CITY = 'Defiance'; 
-    return 'Welcome to ' + CITY;
+    return `Welcome to ${CITY}`;
 };
-
 
 
 
